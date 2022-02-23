51551c0716df32353ad77f0b2068ea07d0a61415
Luca Thoms <luca.thoms@googlemail.com>
2022-02-23 09:09:09 +0100

    Upload my Documents

---
+++ R/JASP Packages (local).R
@@ -0,0 +1,13 @@
+# Installing jaspTools with devtools (this is required onyl once at the start)
+devtools::install_github("jasp-stats/jaspTools")
+library(jaspTools)
+setupJaspTools() # This is required once after every reinstall of jaspTools.
+
+
+# Loading jaspTools
+library(jaspTools)
+library(jaspBase)
+library(jaspGraphs)
+library(jaspResults)
+
+
