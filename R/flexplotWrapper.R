fbf9f372b2a5fe32dbc4ebea75396d8782cf78d7
bruno boutin <b.boutin@uva.nl>
2025-03-31 17:14:49 +0200

    Update wrappers (#360)

--- R/flexplotWrapper.R
+++ R/flexplotWrapper.R
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013-2022 University of Amsterdam
+# Copyright (C) 2013-2025 University of Amsterdam
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,16 +15,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# This is a generated file. Don't change it
+# This is a generated file. Don't change it!
 
 flexplot <- function(
           data = NULL,
-          version = "0.19.2",
+          version = "0.19.3",
           formula = NULL,
           alpha = 0.4,
           bw = FALSE,
           confidence = FALSE,
-          dependent = list(types = "", value = ""),
+          dependent = list(types = list(), value = ""),
           ghost = TRUE,
           intervals = "Quartiles",
           jitx = 0.1,
@@ -51,16 +51,20 @@ flexplot <- function(
    options[["data"]] <- NULL
    options[["version"]] <- NULL
 
+
+   if (!jaspBase::jaspResultsCalledFromJasp() && !is.null(data)) {
+      jaspBase::storeDataSet(data)
+   }
+
    if (!is.null(formula)) {
       if (!inherits(formula, "formula")) {
          formula <- as.formula(formula)
       }
       options$formula <- jaspBase::jaspFormula(formula, data)
    }
-
    optionsWithFormula <- c("dependent", "intervals", "palette", "paneledVars", "theme", "type", "variables")
    for (name in optionsWithFormula) {
       if ((name %in% optionsWithFormula) && inherits(options[[name]], "formula")) options[[name]] = jaspBase::jaspFormula(options[[name]], data)   }
 
-   return(jaspBase::runWrappedAnalysis("jaspDescriptives::flexplot", data, options, version))
+   return(jaspBase::runWrappedAnalysis("jaspDescriptives", "flexplot", "Flexplot.qml", options, version, FALSE))
 }
--- R/raincloudPlotsWrapper.R
+++ R/raincloudPlotsWrapper.R
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013-2024 University of Amsterdam
+# Copyright (C) 2013-2025 University of Amsterdam
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,11 +15,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# This is a generated file. Don't change it
+# This is a generated file. Don't change it!
 
 raincloudPlots <- function(
           data = NULL,
-          version = "0.19.2",
+          version = "0.19.3",
           boxNudge = 0,
           boxOpacity = 0.5,
           boxOutline = "colorPalette",
@@ -28,12 +28,12 @@ raincloudPlots <- function(
           boxWidth = 0.1,
           colorAnyway = TRUE,
           colorPalette = "colorblind",
-          covariate = list(types = "", value = ""),
+          covariate = list(types = list(), value = ""),
           covariatePalette = "viridis",
           customAxisLimits = FALSE,
           customColors = FALSE,
           customSides = FALSE,
-          customizationTable = list(),
+          customizationTable = list(list(levels = "Row 0", name = "data 1", values = "R"), list(levels = "Row 0", name = "data 2", values = 0), list(levels = "Row 0", name = "data 3", values = 0), list(levels = "Row 0", name = "data 4", values = "#00A9E6")),
           dependentVariables = list(types = list(), value = list()),
           heightPlot = 350,
           horizontal = FALSE,
@@ -50,7 +50,7 @@ raincloudPlots <- function(
           meanPosition = "likeBox",
           meanSize = 6,
           numberOfClouds = 1,
-          observationId = list(types = "", value = ""),
+          observationId = list(types = list(), value = ""),
           observationIdLineOpacity = 0.25,
           observationIdLineWidth = 1,
           placeholder1 = 0,
@@ -67,8 +67,8 @@ raincloudPlots <- function(
           pointOpacity = 0.5,
           pointSize = 2.5,
           pointSpread = 0.065,
-          primaryFactor = list(types = "", value = ""),
-          secondaryFactor = list(types = "", value = ""),
+          primaryFactor = list(types = list(), value = ""),
+          secondaryFactor = list(types = list(), value = ""),
           showBox = TRUE,
           showCaption = TRUE,
           showLegend = FALSE,
@@ -94,9 +94,14 @@ raincloudPlots <- function(
    options[["data"]] <- NULL
    options[["version"]] <- NULL
 
+
+   if (!jaspBase::jaspResultsCalledFromJasp() && !is.null(data)) {
+      jaspBase::storeDataSet(data)
+   }
+
    optionsWithFormula <- c("boxOutline", "colorPalette", "covariate", "covariatePalette", "customizationTable", "dependentVariables", "observationId", "primaryFactor", "secondaryFactor", "vioOutline")
    for (name in optionsWithFormula) {
       if ((name %in% optionsWithFormula) && inherits(options[[name]], "formula")) options[[name]] = jaspBase::jaspFormula(options[[name]], data)   }
 
-   return(jaspBase::runWrappedAnalysis("jaspDescriptives::raincloudPlots", data, options, version))
+   return(jaspBase::runWrappedAnalysis("jaspDescriptives", "raincloudPlots", "raincloudPlots.qml", options, version, TRUE))
 }
