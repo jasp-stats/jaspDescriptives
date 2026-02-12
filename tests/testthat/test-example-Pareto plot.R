fb0d2edb99194e90b90969d8f5a7ff00f75b184d
Julian Wuth <145038140+julianwuth@users.noreply.github.com>
2026-02-12 12:43:19 +0100

    Fixing Pareto chart bug (#459)

---
+++ tests/testthat/test-example-Pareto plot.R
@@ -0,0 +1,49 @@
+context("Example: Pareto plot")
+
+# This test file was auto-generated from a JASP example file.
+# The JASP file is stored in the module's examples/ folder.
+
+test_that("Descriptives (analysis 1) results match", {
+
+  # Load from JASP example file
+  jaspFile <- testthat::test_path("..", "..", "examples", "Pareto plot.jasp")
+  opts <- jaspTools::analysisOptions(jaspFile)[[1]]
+  dataset <- jaspTools::extractDatasetFromJASPFile(jaspFile)
+
+  # Encode and run analysis
+  encoded <- jaspTools:::encodeOptionsAndDataset(opts, dataset)
+  set.seed(1)
+  results <- jaspTools::runAnalysis("Descriptives", encoded$dataset, encoded$options, encodedDataset = TRUE)
+
+  plotName <- results[["results"]][["paretoPlots"]][["collection"]][["paretoPlots_jaspColumn2"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "analysis-1_figure-1_jaspcolumn2")
+
+  table <- results[["results"]][["stats"]][["data"]]
+  jaspTools::expect_equal_tables(table,
+    list(0, 9, "jaspColumn2"))
+
+})
+
+test_that("Descriptives (analysis 2) results match", {
+
+  # Load from JASP example file
+  jaspFile <- testthat::test_path("..", "..", "examples", "Pareto plot.jasp")
+  opts <- jaspTools::analysisOptions(jaspFile)[[2]]
+  dataset <- jaspTools::extractDatasetFromJASPFile(jaspFile)
+
+  # Encode and run analysis
+  encoded <- jaspTools:::encodeOptionsAndDataset(opts, dataset)
+  set.seed(1)
+  results <- jaspTools::runAnalysis("Descriptives", encoded$dataset, encoded$options, encodedDataset = TRUE)
+
+  plotName <- results[["results"]][["paretoPlots"]][["collection"]][["paretoPlots_jaspColumn2"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "analysis-2_figure-1_jaspcolumn2")
+
+  table <- results[["results"]][["stats"]][["data"]]
+  jaspTools::expect_equal_tables(table,
+    list(0, 9, "jaspColumn2"))
+
+})
+
